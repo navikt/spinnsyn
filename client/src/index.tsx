d5780a2cc8ee2da3066d12ef7a8208b034dbead0
Jonas Henie <jonas.henie@nav.no>
2020-05-16 13:59:40 +0200

    Add dev decorator

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,14 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import withMenu from './apiMock/decorator/decorator-header-withmenu';
+import megamenu from './apiMock/decorator/decorator-megamenu';
+import footer from './apiMock/decorator/decorator-footer';
+import scripts from './apiMock/decorator/decorator-scripts';
+import skiplinks from './apiMock/decorator/decorator-skiplinks';
+import styles from './apiMock/decorator/decorator-styles';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const init = async () => {
+  if (process.env.NODE_ENV === 'development') {
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_HEADING}}}', withMenu);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_FOOTER}}}', footer);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_STYLES}}}', styles);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SCRIPTS}}}', scripts);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{NAV_SKIPLINKS}}}', skiplinks);
+    document.body.innerHTML = document.body.innerHTML.replace('{{{MEGAMENU_RESOURCES}}}', megamenu);
+
+    // Execute client.js
+    var script = document.createElement('script');
+    script.src = 'https://www.nav.no/dekoratoren/client.js';
+    document.body.appendChild(script);
+  }
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('app'),
+  );
+  serviceWorker.unregister();
+};
+init();
